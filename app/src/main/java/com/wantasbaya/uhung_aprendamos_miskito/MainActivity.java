5ac75f0f142052a73701e399fedd60d34cb00e6f
SamTorr <samuelvtorres94@gmail.com>
2019-11-09 09:51:41 -0600

    creación del proyecto

---
+++ app/src/main/java/com/wantasbaya/uhung_aprendamos_miskito/MainActivity.java
@@ -0,0 +1,28 @@
+package com.wantasbaya.uhung_aprendamos_miskito;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        git init
+        git add .
+        git commit - m "mensaje"
+        git add remote origin url
+        git push origin master
+
+        Actualizar archivos
+
+        git status
+        git add
+        git commit -m "Mensaje"
+        git push origin master
+
+    }
+}
