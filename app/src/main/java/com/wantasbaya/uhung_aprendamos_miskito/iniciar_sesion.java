831f17aa99539d39474151dc762c05b037f5e5c1
SamTorr <samuelvtorres94@gmail.com>
2019-11-09 21:08:30 -0600

    Interfaces

--- app/src/main/java/com/wantasbaya/uhung_aprendamos_miskito/iniciar_sesion.java
+++ app/src/main/java/com/wantasbaya/uhung_aprendamos_miskito/iniciar_sesion.java
@@ -2,7 +2,9 @@ package com.wantasbaya.uhung_aprendamos_miskito;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class iniciar_sesion extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class iniciar_sesion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
+
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
