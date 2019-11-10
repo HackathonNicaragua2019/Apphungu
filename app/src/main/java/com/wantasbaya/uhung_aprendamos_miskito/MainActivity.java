c09b92e938e76b2307eb4e6e2dc48977ec6e9f63
SamTorr <samuelvtorres94@gmail.com>
2019-11-09 21:51:52 -0600

    Cambios de pantalla

--- app/src/main/java/com/wantasbaya/uhung_aprendamos_miskito/MainActivity.java
+++ app/src/main/java/com/wantasbaya/uhung_aprendamos_miskito/MainActivity.java
@@ -8,16 +8,13 @@ import android.os.Bundle;
 import android.view.WindowManager;
 
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn;
+    private Button btnCrearCuenta, IniciarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +26,21 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 
-        int master = 3;
+        btnCrearCuenta = (Button) findViewById(R.id.btn_registro);
+        IniciarSesion = (Button) findViewById(R.id.btn_Sesion);
 
-        btn = (Button) findViewById(R.id.button);
 
-
-        btn.setOnClickListener(new View.OnClickListener() {
+        btnCrearCuenta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this,crear_cuenta.class));
+            }
+        });
+
+        IniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,iniciar_sesion.class));
             }
         });
     }
