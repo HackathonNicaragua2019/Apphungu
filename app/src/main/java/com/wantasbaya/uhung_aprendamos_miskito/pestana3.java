68798e8550d78dc1baf4c2b338b13e0275d6416b
SamTorr <samuelvtorres94@gmail.com>
2019-11-09 21:08:09 -0600

    Interfaces

--- app/src/main/java/com/wantasbaya/uhung_aprendamos_miskito/pestana3.java
+++ app/src/main/java/com/wantasbaya/uhung_aprendamos_miskito/pestana3.java
@@ -1,27 +1,42 @@
 package com.wantasbaya.uhung_aprendamos_miskito;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import static android.app.Activity.RESULT_OK;
 
 
 public class pestana3 extends Fragment {
 
+    ImageView imagen;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
+
+
+
+
         return inflater.inflate(R.layout.fragment_pestana3, container, false);
     }
 
-    public interface OnFragmentInteractionListener {
+     public interface OnFragmentInteractionListener {
     }
+
+
 }
